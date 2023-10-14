df20c56e48c7b7385e2c7b34f297da337acf52b0
Lawren10 <lawrenceanthony70@gmail.com>
2023-10-14 02:34:51 +0100

    removed the messaging functions for now

--- app/api/sendEmail/route.js
+++ app/api/sendEmail/route.js
@@ -1,26 +1,26 @@
-import { NextResponse } from "next/server";
-import * as FormData from "form-data";
-import Mailgun from "mailgun.js";
-const mailgun = new Mailgun(FormData);
-const mg = mailgun.client({
-  username: "api",
-  key: process.env.MAILGUN_API_KEY,
-});
+// import { NextResponse } from "next/server";
+// import * as FormData from "form-data";
+// import Mailgun from "mailgun.js";
+// const mailgun = new Mailgun(FormData);
+// const mg = mailgun.client({
+//   username: "api",
+//   key: process.env.MAILGUN_API_KEY,
+// });
 
-export async function GET() {
-  try {
-    const emailRes = await mg.messages.create(
-      "sandboxf55422edc52b4bd6bf7c10064b959da8.mailgun.org",
-      {
-        from: "Excited User <mailgun@sandboxf55422edc52b4bd6bf7c10064b959da8.mailgun.org>",
-        to: ["lawrenceugo10@gmail.com"],
-        subject: "Hello",
-        html: "<h1>Testing some Mailgun awesomeness!</h1> <p>This is an auto generated message please just allow and sont resopond to it</p>",
-      }
-    );
+// export async function GET() {
+//   try {
+//     const emailRes = await mg.messages.create(
+//       "sandboxf55422edc52b4bd6bf7c10064b959da8.mailgun.org",
+//       {
+//         from: "Excited User <mailgun@sandboxf55422edc52b4bd6bf7c10064b959da8.mailgun.org>",
+//         to: ["lawrenceugo10@gmail.com"],
+//         subject: "Hello",
+//         html: "<h1>Testing some Mailgun awesomeness!</h1> <p>This is an auto generated message please just allow and sont resopond to it</p>",
+//       }
+//     );
 
-    return NextResponse.json(emailRes);
-  } catch (error) {
-    return NextResponse.json(error);
-  }
-}
+//     return NextResponse.json(emailRes);
+//   } catch (error) {
+//     return NextResponse.json(error);
+//   }
+// }
--- app/api/sendSms/route.js
+++ app/api/sendSms/route.js
@@ -1,21 +1,21 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-const accountSid = process.env.TWILIO_ACCOUNT_SID;
-const authToken = proces.env.TWILIO_AUTH_TOKEN;
+// const accountSid = process.env.TWILIO_ACCOUNT_SID;
+// const authToken = proces.env.TWILIO_AUTH_TOKEN;
 
-const smsClient = require("twilio")(accountSid, authToken);
+// const smsClient = require("twilio")(accountSid, authToken);
 
-const messageObj = {
-  body: "Hello from twilio-node testing",
-  to: "+2347046220065", // Text your number
-  from: "+14179295554",
-};
+// const messageObj = {
+//   body: "Hello from twilio-node testing",
+//   to: "+2347046220065", // Text your number
+//   from: "+14179295554",
+// };
 
-export async function GET() {
-  try {
-    const messageRes = await smsClient.messages.create(messageObj);
-    return NextResponse.json(messageRes);
-  } catch (error) {
-    return NextResponse.json(error);
-  }
-}
+// export async function GET() {
+//   try {
+//     const messageRes = await smsClient.messages.create(messageObj);
+//     return NextResponse.json(messageRes);
+//   } catch (error) {
+//     return NextResponse.json(error);
+//   }
+// }
